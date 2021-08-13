59b5b370c02ff0dc785d997d1d78e7bbf522c216
esteban perez <esteban.perez@alteru.co>
2021-08-13 13:43:14 -0500

    prueba alteru

--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,13 @@
+import { useState } from "react"
+
 export const Counter = () =>{
+    const [num, setNum]= useState(5);
     return (
         <>
+        <button onClick={()=>setNum(num + 1)}>+</button>
+        <h1>{num}</h1>
+        <button onClick={()=>setNum(num -1)}>-</button>
+        <button onClick={()=>setNum(5)}>Reset</button>
             
         </>
     )
--- src/fetchapi.js
+++ src/fetchapi.js
@@ -1,6 +1,19 @@
+import { useEffect, useState } from "react";
 
+export const GetMovieList = (year)=>{
+    
 
+    let API =`https://jsonmock.hackerrank.com/api/movies?Year=${year}`;
+    const [movies, setMovie] = useState([]);
+   
 
-export const getMovieList = (year)=>{
-    return undefined;
+    useEffect(() => {
+        fetch(API).then((resp) => resp.json())
+        .then((data)=>{
+            setMovie(data.data);
+            console.log(data);
+        });
+    }, []);   
+     
+    return movies;
 }
